b404ed0061b16d7e08df5c5cfa0ec4ffa13fd7e3
Yash Sharma <yash32860@gmail.com>
2024-11-17 23:00:59 +0530

    project service class implemented

---
+++ src/main/java/com/Project_Management/Service/ProjectService.java
@@ -0,0 +1,25 @@
+package com.Project_Management.Service;
+
+import com.Project_Management.Entity.Chat;
+import com.Project_Management.Entity.Project;
+import com.Project_Management.Entity.User;
+
+import java.util.List;
+
+public interface ProjectService {
+    Project createProject(Project project,User user);
+    List<Project> getProjectByTeam(User user,String category,String tags);
+
+    Project getProjectById(Long projectId);
+
+    void deleteProject(Long projectId,Long userId);
+
+    Project updateProject(Project project,Long projectId);
+
+    void addUserToProject(Long projectId,Long userId);
+
+    void removeUserToProject(Long projectId,Long userId);
+
+    Chat getChatByProjectId(Long projectId);
+
+}
